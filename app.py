7f3b12b2f0e6b27775f6939b615c21b99c7e3e40
Alfa <165712657+Aalfaa@users.noreply.github.com>
2025-06-03 17:19:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
 from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
-def index():
-    return send_from_directory('.', 'index.html')
+def home():
+    return send_from_directory('static', 'index.html')
 
-@app.route('/page2')
-def page2():
-    return send_from_directory('.', 'page2.html')
-
-@app.route('/page3')
-def page3():
-    return send_from_directory('.', 'page3.html')
-
-@app.route('/page4')
-def page4():
-    return send_from_directory('.', 'page4.html')
-
-@app.route('/page5')
-def page5():
-    return send_from_directory('.', 'page5.html')
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
